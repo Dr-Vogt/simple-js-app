032a8edf6800cac7bb00f69f2e3a88368792676b
Dr-Vogt <145719602+Dr-Vogt@users.noreply.github.com>
2023-10-02 13:49:58 -0700

    Started the pokemon list in js. Added 15 pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-let favoriteFood ='Sushi' ;
-document.write(favoriteFood) ;
+let pokemonList = [
+    {name: 'Bulbasaur', height: 74, types: ['Grass','Poison'], ability: ['Overgrow', 'Chlorophyll'] },
+    {name: 'Ivysaur', height: 101, types: ['Grass','Poison'], ability: ['Overgrow', 'Chlorophyll'] },
+    {name: 'Venusaur', height: 204, types: ['Grass','Poison'], ability: ['Overgrow', 'Chlorophyll'] },
+    {name: 'Charmander', height: 60, types: ['Fire'], ability: ['Blaze', 'Solar Power'] },
+    {name: 'Charmeleon', hieght: 113, types: ['Fire'], ability: ['Blaze', 'Solar Power'] },
+    {name: 'Charizard', hieght: 174, types: ['Fire', 'Flying'], ability: ['Blaze', 'Solar Power'] },
+    {name: 'Squirtle', hieght: 55, types: ['Water'], ability: ['Torrent', 'Rain Dish'] },
+    {name: 'Wartortle', hieght: 101, types: ['Water'], ability: ['Torrent', 'Rain Dish'] },
+    {name: 'Blastoise', hieght: 162, types: ['Water'], ability: ['Torrent', 'Rain Dish'] },
+    {name: 'Caterpie', hieght: 30, types: ['Bug'], ability: ['Shield Dust', 'Run Away'] },
+    {name: 'Metapod', hieght: 73, types: ['Bug'], ability: ['Shed Skin'] },
+    {name: 'Butterfree', hieght: 113, types: ['Bug', 'Flying'], ability: ['Compound Eyes', 'Tinted Lens'] },
+    {name: 'Weedle', hieght: 30, types: ['Bug', 'Poison'], ability: ['Shield Dust', 'Run Away'] },
+    {name: 'Kakuna', hieght: 60, types: ['Bug', 'Poison'], ability: ['Shed Skin'] },
+    {name: 'Beedrill', hieght: 101, types: ['Bug', 'Poison'], ability: ['Swarm', 'Sniper'] },
+
+
+
+
+];
